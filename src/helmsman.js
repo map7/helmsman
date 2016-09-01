51edc16eae16dfa372af1050fc33c7709de6a261
Michael Pope <michael@dtcorp.com.au>
2016-09-01 15:08:03 +1000

    Operating the menu system with shortcut keys
    We can use shortcuts for links and states but at the moment we cannot go backwards.

--- src/helmsman.js
+++ src/helmsman.js
@@ -1,6 +1,11 @@
 // Module
 angular.module('helmsman', ['cfp.hotkeys'])
 
+angular.module('helmsman').run(function ($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+});
+
 // Get script path
 scriptPath = document.scripts[document.scripts.length-1].src
 templatePath=(scriptPath).replace("helmsman.js","../templates/helmsman.html")
@@ -22,18 +27,44 @@ menuDirective = function(){
 angular.module('helmsman').directive('menuDirective', menuDirective)
 
 // Controller
-HelmsmanController = ["$scope", "hotkeys", function($scope, hotkeys){
+HelmsmanController = ["$scope", "$state", "hotkeys", function($scope, $state, hotkeys){
     // Add shortcut keys for navigation using function keys
     setShortcuts = function(menu){
         [1,2,3,4,5,6,7,8,9,10,11,12].forEach(setShortcut);
     }
 
+    // Return menu without breadcrumbs
+    menuWithoutBreadcrumbs = function(menu){
+        slimMenu = [];
+
+        menu.forEach(function(item,index){
+            if(item["breadcrumb"] == undefined){
+                slimMenu.push(item);
+            }
+        })
+        
+        return slimMenu;
+    }
+
+    // Set shortcut for all the menu items.
     setShortcut = function(item,index) {
+        menu=menuWithoutBreadcrumbs($scope.items);
+
+        hotkeys.del('f' + item);
+        
         hotkeys.add({
             combo: 'f' +  item,
             description: "",
+            allowIn: ['INPUT', 'SELECT', 'TEXTAREA'],
             callback: function(e) {
-                alert($scope.items[item - 1]["label"]);
+                if(menu[item-1]){
+                    if(menu[item-1]["state"]){
+                        $state.go(menu[item-1]["state"]);
+                    }
+                    if(menu[item-1]["link"]){
+                        $scope.setMenu(menu[item-1]["link"]);
+                    }
+                }
                 e.preventDefault();
             }
         });    
@@ -55,10 +86,7 @@ HelmsmanController = ["$scope", "hotkeys", function($scope, hotkeys){
         }else{
             $scope.setMenu("main")
         }
-
-     
     }
-    
-}]
+}];
 
 angular.module('helmsman').controller('HelmsmanController', HelmsmanController)
