db5c974cf86cc9a27bbb66781db42d09dd04c7fe
Michael Pope <michael@dtcorp.com.au>
2016-09-01 14:27:27 +1000

    Add shortcuts for every menu item

--- src/helmsman.js
+++ src/helmsman.js
@@ -1,5 +1,5 @@
 // Module
-angular.module('helmsman', [])
+angular.module('helmsman', ['cfp.hotkeys'])
 
 // Get script path
 scriptPath = document.scripts[document.scripts.length-1].src
@@ -22,14 +22,31 @@ menuDirective = function(){
 angular.module('helmsman').directive('menuDirective', menuDirective)
 
 // Controller
-HelmsmanController = ["$scope", function($scope){
+HelmsmanController = ["$scope", "hotkeys", function($scope, hotkeys){
+    // Add shortcut keys for navigation using function keys
+    setShortcuts = function(menu){
+        [1,2,3,4,5,6,7,8,9,10,11,12].forEach(setShortcut);
+    }
 
-    // Check menus
+    setShortcut = function(item,index) {
+        hotkeys.add({
+            combo: 'f' +  item,
+            description: "",
+            callback: function(e) {
+                alert($scope.items[item - 1]["label"]);
+                e.preventDefault();
+            }
+        });    
+    }
+    
+    // Check menus exist
     if($scope.menus && $scope.locationToMenu){
+        
         // Set the active menu
         $scope.setMenu = function(menu){
             $scope.heading = menu
             $scope.items = $scope.menus[menu]
+            setShortcuts()
         }
 
         // On refresh find the corresponding menu to the page.
@@ -38,6 +55,8 @@ HelmsmanController = ["$scope", function($scope){
         }else{
             $scope.setMenu("main")
         }
+
+     
     }
     
 }]
