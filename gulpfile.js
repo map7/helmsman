7c119743fd1087ce738309a21fee8c21c8945344
Michael Pope <michael@dtcorp.com.au>
2016-09-07 15:48:02 +1000

    Add gulp-watch ability to auto build
    This way we don't forget to build as it will just watch the files and
    build when I change any file in helmsman

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
 // Include gulp
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    watch = require('gulp-watch');
 
 // Include plugins
 var concat = require('gulp-concat');
 
 // Concatenate JS Files
 gulp.task('scripts', function() {
-    return gulp.src(['app/bower_components/mousetrap/mousetrap.js',
+    return watch(['app/bower_components/mousetrap/mousetrap.js',
                      'app/bower_components/angular-hotkeys/src/hotkeys.js',
-                     'src/*.js'])
+                  'src/*.js'], {ignoreInitial: false})
       .pipe(concat('helmsman.js'))
       .pipe(gulp.dest('build'));
 });
 
 // Copy html files
 gulp.task('html', function() {
-    return gulp.src(['src/*.html'])
+    return watch('src/*.html', {ignoreInitial: false})
       .pipe(gulp.dest('build'));
 });
 
