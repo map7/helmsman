92bb608ae167b38aa50c3a4afff96809001fc3b5
Michael Pope <michael@dtcorp.com.au>
2016-09-14 17:27:30 +1000

    Move template to text
    This is due to the URL not being dynamically created correctly. I have
    to put it in as text now.

--- src/helmsman_directive.js
+++ src/helmsman_directive.js
@@ -7,7 +7,22 @@ helmsmanDirective = function(){
             menus: '=',
             locationToMenu: '='
         },
-        templateUrl: templatePath,
+        template: '<div>' +
+  '<div class="helmsman-breadcrumb">' +
+    '<span class="helmsman-breadcrumb-key">{{helmsman_back_shortcut}}</span>' +
+    '<span ng-repeat="item in items" ng-if="item.breadcrumb">' +
+      '<a href="" ng-click="setMenu(item.breadcrumb)" ng-if="item.breadcrumb">{{item.label}}</a> >' + 
+    '</span>' + 
+  '</div>' + 
+  '<div class="helmsman-menu">' + 
+    '<div class="helmsman-heading" ng-if="heading">{{heading}}</div>' + 
+    '<li ng-repeat="item in items" ng-if="!item.breadcrumb">' + 
+      '<span class="helmsman-key">{{item.key}}</span>' + 
+      '<a href="" ng-click="setMenu(item.link)" ng-if="item.link">{{item.label}}</a>' + 
+      '<a ui-sref="{{item.state}}" ng-if="item.state">{{item.label}}</a>' + 
+    '</li>' + 
+  '</div>' + 
+'</div>',
         controller: HelmsmanController
     }
 };
