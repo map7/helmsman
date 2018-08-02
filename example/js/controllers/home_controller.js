28a6138c4a3ae2f8bbac668d2db122ee3cd05fab
Michael Pope <michael@dtcorp.com.au>
2018-08-02 15:16:51 +1000

    Change example menu to include a missing item

--- example/js/controllers/home_controller.js
+++ example/js/controllers/home_controller.js
@@ -1,60 +1,61 @@
 home = ["$rootScope", "$scope", "$state", "$stateParams", "LedgerService", function($rootScope, $scope, $state, $stateParams, LedgerService){
-    $scope.ledgerService = LedgerService;
+  $scope.ledgerService = LedgerService;
 
-    $rootScope.$state = $state;
-    $rootScope.$stateParams=$stateParams;
+  $rootScope.$state = $state;
+  $rootScope.$stateParams=$stateParams;
 
-    $scope.setLedger = function(value){
-        LedgerService.setLedger(value);
-    }
+  $scope.setLedger = function(value){
+    LedgerService.setLedger(value);
+  }
 
-    // --------------------------------------------------------------------------------
-    // Menu related stuff
-    // --------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------
+  // Menu related stuff
+  // --------------------------------------------------------------------------------
 
-    // Build the menu
-    $scope.menus = {
-        main: [
-            {label: "Costings Menu...",  link: "costings"},
-            {label: "Settings Menu...",  link: "Settings"}
-        ],
-        costings:  [
-            {label: "Main",      breadcrumb: "main"},
-            {label: "Invoices",  state: "invoices"},
-            {label: "Invoices",  state: "invoices"},
-            {label: "Invoices",  state: "invoices"},
-            {label: "Invoices",  state: "invoices"},
-            {label: "Invoices",  state: "invoices"},
-            {label: "Invoices",  state: "invoices"},
-            {label: "Invoices",  state: "invoices"},
-            {label: "Invoices",  state: "invoices"},
-            {label: "Invoices",  state: "invoices"},
-            {label: "Invoices",  state: "invoices"},
-            {label: "Invoices",  state: "invoices"},
-            {label: "Invoices",  state: "invoices"},
-            {label: "Invoices",  state: "invoices"},
-            {label: "Invoices",  state: "invoices"}
-        ],
-        Settings:  [
-            {label: "Main",      breadcrumb: "main"},
-            {label: "Accounting Menu...",link: "Accounting"},
-            {label: "Account",   state: "account"}
-        ],
-        Accounting: [
-            {label: "Main",      breadcrumb: "main"},
-            {label: "Settings",  breadcrumb: "Settings"},
-            {label: "Profile",   state: "profile"}
-        ]
-    }
+  // Build the menu
+  $scope.menus = {
+    main: [
+      {label: "Costings Menu...",  link: "costings"},
+      {label: "Settings Menu...",  link: "Settings"},
+    ],
+    costings:  [
+      {label: "Main",      breadcrumb: "main"},
+      {label: "Invoices",  state: "invoices"},
+      {label: "Invoices",  state: "invoices"},
+      {label: "Invoices",  state: "invoices"},
+      {label: "Invoices",  state: "invoices"},
+      {label: "Invoices",  state: "invoices"},
+      {label: "Invoices",  state: "invoices"},
+      {label: "Invoices",  state: "invoices"},
+      {label: "Invoices",  state: "invoices"},
+      {label: "Invoices",  state: "invoices"},
+      {label: "Invoices",  state: "invoices"},
+      {label: "Invoices",  state: "invoices"},
+      {label: "Invoices",  state: "invoices"},
+      {label: "Invoices",  state: "invoices"},
+      {label: "Invoices",  state: "invoices"}
+    ],
+    Settings:  [
+      {label: "Main",      breadcrumb: "main"},
+      {label: "Accounting Menu...",link: "Accounting"},
+      {label: "Account",   state: "account"},
+      {label: "Missing state",     state: "missing"}
+    ],
+    Accounting: [
+      {label: "Main",      breadcrumb: "main"},
+      {label: "Settings",  breadcrumb: "Settings"},
+      {label: "Profile",   state: "profile"}
+    ]
+  }
 
-    // // On the refresh
-    // // page -> menu
-    // $scope.locationToMenu = {
-    //     "#/account": "Settings",
-    //     "#/profile": "Accounting"
-    // }
-    
-    // --------------------------------------------------------------------------------
+  // // On the refresh
+  // // page -> menu
+  // $scope.locationToMenu = {
+  //     "#/account": "Settings",
+  //     "#/profile": "Accounting"
+  // }
+  
+  // --------------------------------------------------------------------------------
 }]
 
 angular.module('app').controller('home', home)
