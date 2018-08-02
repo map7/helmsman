29197e774f4d4c88531add044eba24e66c2026a7
Michael Pope <michael@dtcorp.com.au>
2018-08-02 15:42:12 +1000

    Revert "NG-Annotate module for minification"
    This reverts commit 3dc12b94ab1c0bd7b5d513f2bdcc278e578d3b60.
    
    I don't use this anymore because of a bug

--- src/helmsman.js
+++ src/helmsman.js
@@ -1,10 +1,10 @@
 // Module
 angular.module('helmsman', ['cfp.hotkeys'])
 
-angular.module('helmsman').run(["$rootScope", "$state", "$stateParams", function ($rootScope, $state, $stateParams) {
+angular.module('helmsman').run(function ($rootScope, $state, $stateParams) {
     $rootScope.$state = $state;
     $rootScope.$stateParams = $stateParams;
-}]);
+});
 
 // Get script path
 scriptPath = document.scripts[document.scripts.length-1].src
